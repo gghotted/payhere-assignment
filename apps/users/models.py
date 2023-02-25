321501c74a9bbf22026f6e7ed913ac54a944a68c
gghotted <gghotted2@naver.com>
2023-02-25 19:51:31 +0900

    Feat: User 모델 메니저 추가

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,14 @@
 from core.models import BaseModel
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password):
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save()
+        return user
 
 
 class User(BaseModel, AbstractUser):
@@ -9,6 +17,8 @@ class User(BaseModel, AbstractUser):
         verbose_name='이메일',
         unique=True,
     )
+
+    objects = UserManager()
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
