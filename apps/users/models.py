ee2c461aea87640583f24ed784b66db0f0adfa74
gghotted <gghotted2@naver.com>
2023-02-25 18:53:11 +0900

    Feat: User 모델 추가

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,14 @@
+from core.models import BaseModel
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(BaseModel, AbstractUser):
+    username = None
+    email = models.EmailField(
+        verbose_name='이메일',
+        unique=True,
+    )
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
