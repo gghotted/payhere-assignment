fb91f1a17ed5e104893dcbe3f1654dbe7b5eb74e
gghotted <gghotted2@naver.com>
2023-02-26 11:17:26 +0900

    Feat: AccountBook 모델 추가

--- apps/account_books/models.py
+++ apps/account_books/models.py
@@ -1,3 +1,21 @@
+from core.models import BaseModel
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 from django.db import models
 
-# Create your models here.
+
+class AccountBook(BaseModel):
+    user = models.ForeignKey(
+        verbose_name='유저',
+        to=get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='account_books',
+    )
+    name = models.CharField(
+        verbose_name='이름',
+        max_length=64,
+        validators=[
+            MinLengthValidator(3),
+            MaxLengthValidator(20),
+        ]
+    )
