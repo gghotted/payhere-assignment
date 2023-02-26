3af82d567ed4a49d370b9d8b219bdf6790fe1c0b
gghotted <gghotted2@naver.com>
2023-02-27 04:34:09 +0900

    Feat: docker-compose setting

--- apps/config/settings/base.py
+++ apps/config/settings/base.py
@@ -10,18 +10,30 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import json
 from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+SECRET_DIR = BASE_DIR / '.secrets'
+
+COMMON_SECRET_FILE = SECRET_DIR / 'common.json'
+COMMON_SECRET = json.loads(COMMON_SECRET_FILE.read_text())
+
+LOCAL_SECRET_FILE = SECRET_DIR / 'local.json'
+LOCAL_SECRET = json.loads(LOCAL_SECRET_FILE.read_text())
+
+PRODUCT_SECRET_FILE = SECRET_DIR / 'product.json'
+PRODUCT_SECRET = json.loads(PRODUCT_SECRET_FILE.read_text())
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7rr0js*h6gd2%$ua$=v!%fp2pctqrs%k2js6+p)r)$=u%p-v$6'
+SECRET_KEY = COMMON_SECRET['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,6 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
 
@@ -112,9 +125,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -125,6 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
